e4bdd6bc72781bff1c4b0ddd83c4385371fb735d
rickysa007 <rickysa007@gmail.com>
2022-02-07 04:58:49 +0800

    sucessful masking

--- RNN_model.py
+++ RNN_model.py
@@ -2,11 +2,12 @@ import absl.logging
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
 import shutil
+import tensorflow as tf
+import tensorflow.keras.backend as K
 from tensorflow import keras
 from tensorflow.keras.layers import GRU, Dense
-from tensorflow.keras.layers import Bidirectional, BatchNormalization
+from tensorflow.keras.layers import Bidirectional, BatchNormalization, Masking
 from tensorflow.keras.layers import TimeDistributed, RepeatVector
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
@@ -33,21 +34,21 @@ def create_input(split_portion=0.8, num_of_type=1):
     type_train = [ [] for i in range(num_of_type+1)]
     type_test = [ [] for i in range(num_of_type+1)]
 
-    for i in range(len(data_GP)):
+    for i in range(len(data_meta_GP)):
 
-        if 'Ia' in data_GP[i][-2]:
+        if 'Ia' in data_meta_GP[i][-2]:
             claimedtype.append(0)
-        if 'IIP' in data_GP[i][-2]:
+        if 'IIP' in data_meta_GP[i][-2]:
             claimedtype.append(1)
 
     for i in range(len(claimedtype)):
 
-        input[0].append(list(data_GP[i][0:-3]))
+        input[0].append(list(data_GP[i]))
 
         if claimedtype[i] == 0:
-            input[1].append(list(data_GP[i][0:-3]))
+            input[1].append(list(data_GP[i]))
         if claimedtype[i] == 1:
-            input[2].append(list(data_GP[i][0:-3]))
+            input[2].append(list(data_GP[i]))
 
     for i in range(len(input)):
         input[i] = np.array(input[i])
@@ -56,10 +57,10 @@ def create_input(split_portion=0.8, num_of_type=1):
         input_test[i]  = input[i][int(split_portion*len(input[i])):]
 
         input[i] = input[i].transpose((0, 2, 1))
-        input_train[i] = input_train[i].transpose((0, 2, 1))
-        input_test[i] = input_test[i].transpose((0, 2, 1))
-        '''input_train[i] = K.cast_to_floatx(input_train[i].transpose((0, 2, 1)))
-        input_test[i] = K.cast_to_floatx(input_test[i].transpose((0, 2, 1)))'''
+        '''input_train[i] = input_train[i].transpose((0, 2, 1))
+        input_test[i] = input_test[i].transpose((0, 2, 1))'''
+        input_train[i] = K.cast_to_floatx(input_train[i].transpose((0, 2, 1)))
+        input_test[i] = K.cast_to_floatx(input_test[i].transpose((0, 2, 1)))
 
         type_train[i] = claimedtype[:int(split_portion*len(input[i]))]
         type_test[i]  = claimedtype[int(split_portion*len(input[i])):]
@@ -69,26 +70,21 @@ def create_input(split_portion=0.8, num_of_type=1):
 
     return input, input_train, input_test, type_train, type_test
 
-def masking(input_tmp, split): # Poor function definition, input_tmp is problematic, split problem
+def masking(input_tmp, split):
 
     weight = []
     input_tmp = input_tmp
 
     for i in range(input_tmp.shape[0]):
         tmp1 = [1 for j in range(data_GP[i+split][-3])]
-        #print(np.array(tmp1).shape)
         tmp2 = [0 for j in range(input_tmp.shape[1] - data_GP[i+split][-3])]
         tmp = np.hstack((tmp1, tmp2))
         weight.append(tmp)
 
     weight = np.array(weight)
-    #print(weight.shape)
 
     mask = [weight for i in range(input_tmp.shape[2])]
-    #print(np.array(mask).shape)
     mask = np.transpose(mask, (1, 2, 0))
-    #print(np.array(mask).shape)
-    #print(mask)
 
     return mask
 
@@ -100,6 +96,14 @@ def custom_loss(y_true, y_pred, mask):
     y_pred_masked = tf.math.multiply(y_pred, mask[:,:,1:last_band_id])
     
     mse = tf.keras.losses.mean_squared_error(y_true = y_true_masked[:,:,1:last_band_id], y_pred = y_pred_masked[:,:,:])
+
+    return mse
+
+def custom_loss2(y_true, y_pred):
+
+    last_band_id = int((data_GP.shape[1] - 3 - 1)/2)+1
+    
+    mse = tf.keras.losses.mean_squared_error(y_true = y_true[:,:,1:last_band_id], y_pred = y_pred[:,:,:])
 
     return mse
 
@@ -138,6 +142,43 @@ def rnnae(input):
 
     return autoencoder, encoder
 
+def rnnae2(input):
+
+    input_seq = keras.Input(shape=(input[0].shape[1], input[0].shape[2]))
+    input_mask = keras.Input(shape=(input[0].shape[1], input[0].shape[2]))
+    #input_conc_phase = keras.Input(shape=(input[0].shape[1], 1))
+
+    x = Masking(mask_value=0., input_shape=(185, 7))(input_seq)
+    x = BatchNormalization()(x)
+    x = Bidirectional(GRU(185, activation='tanh', return_sequences=True))(x)
+    x = BatchNormalization()(x)
+    x = Bidirectional(GRU(50, activation='tanh', return_sequences=True))(x)
+    x = BatchNormalization()(x)
+    encoded = Bidirectional(GRU(6, activation='tanh', return_sequences=False))(x)
+
+    x = RepeatVector(input[0].shape[1])(encoded)
+    #merged = concatenate([x, input_conc_phase], axis=-1)
+    #x = BatchNormalization()(merged)
+    x = BatchNormalization()(x)
+    x = Bidirectional(GRU(50, activation='tanh', return_sequences=True))(x)
+    x = BatchNormalization()(x)
+    x = Bidirectional(GRU(185, activation='tanh', return_sequences=True))(x)
+    x = BatchNormalization()(x)
+    decoded = TimeDistributed(Dense(input[0].shape[2]-3-1))(x)
+
+    #autoencoder = keras.Model([input_seq, input_mask, input_conc_phase], decoded)
+    #autoencoder = keras.Model([input_seq, input_mask], decoded)
+    autoencoder = keras.Model(input_seq, decoded)
+    autoencoder.add_loss(custom_loss2(input_seq, decoded))
+    encoder = keras.Model(input_seq, encoded)
+
+    opt = Adam(learning_rate=0.0001)
+
+    autoencoder.compile(optimizer=opt, loss=None)
+    #autoencoder.summary()
+
+    return autoencoder, encoder
+
 def rnnae_train(autoencoder, input_tmp, mask_tmp):
 
     callbacks = EarlyStopping(monitor='val_loss', min_delta=0, patience=50,
@@ -146,7 +187,7 @@ def rnnae_train(autoencoder, input_tmp, mask_tmp):
 
     history = autoencoder.fit(x=[input_tmp, mask_tmp], y=None,
                             validation_split = 0.1,
-                            epochs=1000,
+                            epochs=2,
                             verbose=1,
                             callbacks=[callbacks])
 
@@ -158,6 +199,26 @@ def rnnae_train(autoencoder, input_tmp, mask_tmp):
     os.chdir('/home/ricky/RNNAE')
     #os.chdir(r'C:\\Users\\ricky\\FYP\\RNNAE_public')
     plt.savefig('training history.pdf')
+
+    return
+
+def rnnae_train2(autoencoder, input_tmp):
+
+    callbacks = EarlyStopping(monitor='val_loss', min_delta=0, patience=50,
+                       verbose=0, mode='min', baseline=None,
+                       restore_best_weights=True)
+
+    history = autoencoder.fit(x=input_tmp, y=None,
+                            validation_split = 0.1,
+                            epochs=2,
+                            verbose=1,
+                            callbacks=[callbacks])
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.grid()
+    plt.ylim(0, 0.05)
+    plt.show()
 
     return
 
@@ -248,8 +309,8 @@ def reconstruction_graph(input_tmp, yhat, split, filters=['u', 'g', 'i']):
 def main():
 
     input, input_train, input_test, type_train, type_test = create_input()
-    mask_train = masking(input_train[0], 0)
-    mask_test = masking(input_test[0], int(0.8*(data_GP.shape[0])))
+    '''mask_train = masking(input_train[0], 0)
+    mask_test = masking(input_test[0], int(0.8*(data_GP.shape[0])))'''
 
     os.chdir('/home/ricky/RNNAE/RNN_npy')
     #os.chdir(r'C:\\Users\\ricky\\FYP\\RNNAE_public\\RNN_npy')
@@ -258,11 +319,11 @@ def main():
     np.save('input_test.npy', np.array(input_test, dtype=object))
     np.save('type_train.npy', np.array(type_train, dtype=object))
     np.save('type_test.npy', np.array(type_test, dtype=object))
-    np.save('mask_train.npy', np.array(mask_train, dtype=object))
-    np.save('mask_test.npy', np.array(mask_test, dtype=object))
+    '''np.save('mask_train.npy', np.array(mask_train, dtype=object))
+    np.save('mask_test.npy', np.array(mask_test, dtype=object))'''
 
-    autoencoder, encoder = rnnae(input)
-    rnnae_train(autoencoder, input_train[0], mask_train)
+    autoencoder, encoder = rnnae2(input)
+    rnnae_train2(autoencoder, input_train[0])
 
     autoencoder.save('/home/ricky/RNNAE/RNN_autoencoder_model')
     encoder.save('/home/ricky/RNNAE/RNN_encoder_model')
